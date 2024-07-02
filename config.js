f43c47bc4ccb66154a06aae620723d00b56c8b1f
MonovanShop <164271382+MonovanShop@users.noreply.github.com>
2024-07-01 23:41:30 -0600

    Update config.js

--- config.js
+++ config.js
@@ -12,12 +12,12 @@ import { en, es, id, ar, pt } from "./lib/idiomas/total-idiomas.js"
 // • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • •
 // [ES] > Agrega el número que será Bot y los que serán propietarios.
 global.owner = [
-["5215625768269", 'Monkey Shop', true], //Capibot Premium
-["525625768269", 'Monkey Shop', true], //Nekotina bot
-["5215625060788"]]
+["521⁠8661398209", 'Monkey Shop', true], //Capibot Premium
+["521⁠8661398209", 'Monkey Shop', true], //Nekotina bot
+["521⁠8661398209"]]
 
-global.mods = ["5215625768269"]
-global.prems = ["5215625768269"]
+global.mods = ["521⁠8661398209"]
+global.prems = ["521⁠8661398209"]
 // • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • •
 
 // • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • •
@@ -112,9 +112,9 @@ global.moment = moment
 // [ES] > Agregate a ti, colaboradores o ayudates, aparecerá en el comando de lista de contactos.
 // [EN] > Adding yourself, collaborators or helpers will appear in the contact list command.
 global.official = [ // Agregate si eres Owner
-["5215625768269", 'Monkey Bot', 1], 
-["525625768269", 'Anthony', 1],  
-["5215615060788", 'Capibot', 1]]
+["521⁠8661398209", 'Monkey Bot', 1], 
+["521⁠8661398209", 'Anthony', 1],  
+["521⁠8661398209", 'Capibot', 1]]
 
 global.mail = '' // Add email
 global.desc = '' // Add short description (20 caractres max)
@@ -122,8 +122,8 @@ global.desc2 = '' // Add long description (90 caractres max) (Este parámetro se
 global.country = '' // Add country, example: 🇪🇨
 // • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • •
 
-global.packname = "MONKEY BOY"
-global.author = "MONOVAN SHOP"
+global.packname = "⁠themonarch bot"
+global.author = "⁠themonarch⁠"
 
 // • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • •
 // [ES] > INFORMACIÓN DE VERSIONES DEL BOT, POR FAVOR 
@@ -131,7 +131,7 @@ global.author = "MONOVAN SHOP"
 // [EN] > BOT VERSION INFORMATION, PLEASE KEEP THIS UNCHANGED, WE STRIVE DAILY TO PROVIDE YOU WITH A BOT FOR THE COMMUNITY, BE GRATEFUL
 global.vs = "1.7.0"
 global.vsJB = "2.5 (Beta)"
-global.gt = "MONKEY"
+global.gt = "⁠themonarch"
 
 // • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • •
 
@@ -157,9 +157,9 @@ global.eg = '╰⊱💚⊱ *𝙀́𝙓𝙄𝙏𝙊 | 𝙎𝙐𝘾𝘾𝙀𝙎�
 global.exito = eg
 
 // • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • •
-global.wm = "MONKEY"
-global.igfg = "MONKEY"
-global.nomorown = "5215625768269"
+global.wm = "⁠themonarch"
+global.igfg = "⁠themonarch"
+global.nomorown = "521⁠8661398209"
 global.pdoc = ["application/vnd.openxmlformats-officedocument.presentationml.presentation", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "application/vnd.openxmlformats-officedocument.wordprocessingml.document", "application/vnd.ms-excel", "application/msword", "application/pdf", "text/rtf"]
 // • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • •
 
