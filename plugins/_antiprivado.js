c99eddebc0b36b29342980e7e039d9d1f123702a
MonovanShop <164271382+MonovanShop@users.noreply.github.com>
2024-07-01 23:39:51 -0600

    Update _antiprivado.js

--- plugins/_antiprivado.js
+++ plugins/_antiprivado.js
@@ -6,8 +6,8 @@ export async function before(m, {conn, isAdmin, isBotAdmin, isOwner, isROwner}) 
   const chat = global.db.data.chats[m.chat];
   const bot = global.db.data.settings[this.user.jid] || {};
   if (bot.antiPrivate && !isOwner && !isROwner) {
-    await m.reply(`*[❗] Hola @${m.sender.split`@`[0]}, Esta prohibido hablar al privado del bot pero si deseas comprarlo habla al siguiente numero \nhttps://wa.me/+5215615060799
-    *`, false, {mentions: [m.sender]});
+    await m.reply(`*[❗] Hola @${m.sender.split`@`[0]}, - Hola!!
+El bot no esta disponible para adquirir tus pedidos, te veo en el grupo, buen día. 💗🛍️*`, false, {mentions: [m.sender]});
     await this.updateBlockStatus(m.chat, 'block');
   }
   return !1;
