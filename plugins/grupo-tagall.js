28fedcbb8708310b636d1804accf94fb0e311e9d
MonovanShop <164271382+MonovanShop@users.noreply.github.com>
2024-07-01 23:37:56 -0600

    Update grupo-tagall.js

--- plugins/grupo-tagall.js
+++ plugins/grupo-tagall.js
@@ -4,11 +4,11 @@ global.dfail('admin', m, conn)
 throw false
 }
 let pesan = args.join` `
-let oi = `@𝕞𝕠𝕟𝕠𝕧𝕒𝕟 ${pesan}`
-let teks = `*ACTIVENSE ADORNOS 🤬*\n${oi}\n\n*🚨𝙈𝙚𝙣𝙘𝙞𝙤𝙣𝙚𝙨*\n`
+let oi = `@𝕥𝕙𝕖𝕞𝕠𝕟𝕒𝕣𝕔𝕙 ${pesan}`
+let teks = `*⁠ATENCIÓN, POR FAVOR 🤬*\n${oi}\n\n*🚨𝙈𝙚𝙣𝙘𝙞𝙤𝙣𝙚𝙨*\n`
 for (let mem of participants) {
-teks += `✨ @${mem.id.split('@')[0]}\n`}
-teks += `𝑴𝑶𝑵𝑲𝑬𝒀 𝑩𝒀 𝑴𝑶𝑵𝑶𝑽𝑨𝑵 𝑽𝑬𝑵𝑻𝑨𝑺`
+teks += `⁠⚠️‼️ @${mem.id.split('@')[0]}\n`}
+teks += `𝑻𝑯𝑬𝑴𝑶𝑵𝑨𝑹𝑪𝑯 𝑩𝑶𝑻 𝑩𝒀 𝑴𝑶𝑵𝑶𝑽𝑨𝑵 𝑽𝑬𝑵𝑻𝑨𝑺`
 conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )  
 }
 handler.command = /^(tagall|invocar|invocacion|todos|invocación)$/i
