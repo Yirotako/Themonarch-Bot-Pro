f630cab80106a6c0bef8a8033e97982c2d4799df
MonovanShop <164271382+MonovanShop@users.noreply.github.com>
2024-07-03 21:17:53 -0600

    Add files via upload

--- plugins/rpg-verificar.js
+++ plugins/rpg-verificar.js
@@ -101,7 +101,7 @@ externalAdReply: {
 title: wm,
 body: '🌟 Puede modificar su registro antes de finalizar',
 thumbnailUrl: pp, 
-sourceUrl: 'https://www.instagram.com/vsebas.y',
+sourceUrl: 'https://chat.whatsapp.com/EOyJAfDmfP8A501vSBzmPV',
 mediaType: 1,
 showAdAttribution: true,
 renderLargerThumbnail: true
@@ -581,7 +581,7 @@ externalAdReply: {
 title: wm,
 body: user.name,
 thumbnailUrl: pp, 
-sourceUrl: 'https://www.instagram.com/vsebas.y',
+sourceUrl: 'https://chat.whatsapp.com/EOyJAfDmfP8A501vSBzmPV',
 mediaType: 1,
 showAdAttribution: true,
 renderLargerThumbnail: true
