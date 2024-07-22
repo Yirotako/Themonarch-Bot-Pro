c6290117efb1df102bc53aca780f02810babb0cf
Yirotako <167583370+Yirotako@users.noreply.github.com>
2024-07-22 14:38:53 -0600

    _antiprivado.js

--- plugins/_antiprivado.js
+++ plugins/_antiprivado.js
@@ -7,7 +7,7 @@ export async function before(m, {conn, isAdmin, isBotAdmin, isOwner, isROwner}) 
   const bot = global.db.data.settings[this.user.jid] || {};
   if (bot.antiPrivate && !isOwner && !isROwner) {
     await m.reply(`*[❗] Hola @${m.sender.split`@`[0]}, - Hola!!
-El bot no esta disponible para adquirir tus pedidos, te veo en el grupo, buen día. 💗🛍️*`, false, {mentions: [m.sender]});
+El bot no esta disponible para adquirir tus pedidos, te veo en el grupo, buen día. Abre este enlace para unirte a mi grupo de WhatsApp: https://chat.whatsapp.com/KRnesA7HbidIa3va6X8un8 💗🛍️*`, false, {mentions: [m.sender]});
     await this.updateBlockStatus(m.chat, 'block');
   }
   return !1;
